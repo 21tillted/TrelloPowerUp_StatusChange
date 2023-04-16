5b9e041ccdeae4a6c68f3752ac0e1e9446ed3722
21tillted <tilltb.brandt@gmail.com>
2023-04-16 21:49:45 +0200

    newVersion

--- PythonAPISide/Trellobboard.py
+++ PythonAPISide/Trellobboard.py
@@ -1,22 +1,33 @@
 from trello import TrelloClient
-from 0Auth import create_oauth_token
+import json
 
-myapi_key='f9274211f281c4ceba5ce109481d25c5'
-myapi_secret='4b84310cca4ec4fb2d8138c65811dcfc04fdaaa3d3c44b6187820dc37fc38c5f'
+steamID = "STEAM_0:0:121430886"
 
-create_oauth_token(myapi_key, myapi_secret)
+myApi_key='f9274211f281c4ceba5ce109481d25c5'
+myApi_secret='4b84310cca4ec4fb2d8138c65811dcfc04fdaaa3d3c44b6187820dc37fc38c5f'
+myToken='ATTAedd540d14edadf2f5eac6bb4570dec3fdb49e968f7511c152bf7f816838e0b62C0364953'
+token_secret=''
 
 client = TrelloClient(
-    api_key= myapi_key,
-    api_secret= myapi_secret,
-    token='your-oauth-token-key',
-    token_secret='your-oauth-token-secret'
+    api_key= myApi_key,
+    api_secret= myApi_secret,
+    token= myToken,
+    #token_secret='your-oauth-token-secret'
 )
 
 all_boards = client.list_boards()
 last_board = all_boards[-1]
 
+currentBoard = client.get_board('8hSBNmno')
+cards = currentBoard.get_cards('all','all')
 
-print(last_board.name)
+for card in cards:
+    comment = json.dumps(card.comments) 
+    
+    if comment.get('SteamID') == steamID:
+        print(card.name)
+        print("true")
+
+print(currentBoard.name)
 
 
