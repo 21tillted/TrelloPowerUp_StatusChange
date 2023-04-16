091a15c1593a8f41996ff76ccbbb7a17a5c2bb47
21tillted <tilltb.brandt@gmail.com>
2023-04-16 23:37:13 +0200

    Update Trellobboard.py

--- PythonAPISide/Trellobboard.py
+++ PythonAPISide/Trellobboard.py
@@ -1,6 +1,6 @@
 from trello import TrelloClient
 from trello.customfield import CustomField, CustomFieldText, CustomFieldCheckbox, CustomFieldNumber, CustomFieldDate, CustomFieldList
-import json
+import pandas as pd
 
 steamID = "STEAM_0:0:121430886"
 
@@ -20,14 +20,34 @@ all_boards = client.list_boards()
 last_board = all_boards[-1]
 
 currentBoard = client.get_board('8hSBNmno')
-cards = currentBoard.get_cards('all','all')
+#cards = currentBoard.get_cards('all','all')
 
-for card in cards:
-    comment = json.dumps(card.comments) 
-    card.customFields.
-    if comment.get('SteamID') == steamID:
-        print(card.name)
-        print("true")
+# Get all the list in the board 
+all_lists = currentBoard.open_lists() 
+df = pd.DataFrame()
+# Get list name and cards in it (Card name) 
+for list in all_lists: 
+ for card in list.list_cards(): 
+   dict = {} 
+   df1 = pd.DataFrame() 
+   if not card.closed: 
+     dict['Card Name']=[card.name]
+     dict['List Name']=[list.name] 
+     # Get the custom fields
+     for custom_fields in card.custom_fields: 
+       dict[custom_fields.name]= [custom_fields.value]
+ 
+     # Create a data frame with card ,list , custom field in card
+     df1 = pd.DataFrame(dict) 
+     # Merge the dataframe in order to add all card's custom fields
+     df =pd.concat([df, df1], ignore_index=True, sort=False) 
+
+
+#Forlater
+#Add comment
+#card.comment("Dui faucibus in ornare quam viverra orci sagittis.")
+#changeLabel(Rank)
+labelsOfCard = card.labels
 
 print(currentBoard.name)
 
