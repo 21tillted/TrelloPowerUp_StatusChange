bdb87b4010cbc2f66ca088f70e53ccd7d1824310
21tillted <tilltb.brandt@gmail.com>
2023-04-17 14:39:59 +0200

    Squashed commit of the following:
    commit dfbbb9c70b67ad49ac1f873cd0b212ddb3023ead
    Merge: 043aec3 c1084b6
    Author: 21tillted <tilltb.brandt@gmail.com>
    Date:   Mon Apr 17 14:37:55 2023 +0200
    
        Merge branch 'main' into v1_TrelloSide
    
    commit 043aec3f5a00882e5c80b4d8c13a80568a9456cf
    Author: 21tillted <tilltb.brandt@gmail.com>
    Date:   Mon Apr 17 14:36:26 2023 +0200
    
        Update Main.py
    
    commit c1084b6d0c3eb18e9f66b383b0002b2209dac6ab
    Author: 21tillted <tilltb.brandt@gmail.com>
    Date:   Mon Apr 17 14:36:08 2023 +0200
    
        test

--- PythonAPISide/Main.py
+++ PythonAPISide/Main.py
@@ -1,3 +1,16 @@
-from trello import TrelloClient
+import ProDemote
+
+ProDeInstance = ProDemote
 
 steamID = "STEAM_0:0:121430886"
+
+#Eingabe von SteamID
+print("bitte die SteamID")
+usersteamID = input()       
+#Ausgabe von Name& Rang
+print(ProDeInstance.getNameAndRankFromSteamID(usersteamID)) 
+#Welchen Rang soll er gesetzt werden?
+selectedRank = input()
+#Wer ändert den Rang?
+changeingUser = input()
+ProDeInstance.changeRankOfCard(selectedRank)   
