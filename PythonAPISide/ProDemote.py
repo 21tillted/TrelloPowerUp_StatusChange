76bdf1a61650ae15a1ba751e16dd082fded4cc07
21tillted <tilltb.brandt@gmail.com>
2023-04-17 10:25:26 +0200

    new

--- PythonAPISide/ProDemote.py
+++ PythonAPISide/ProDemote.py
@@ -1,18 +1,17 @@
 from trello import TrelloClient
 from trello.customfield import CustomField, CustomFieldText, CustomFieldCheckbox, CustomFieldNumber, CustomFieldDate, CustomFieldList
 import pandas as pd
+import config
+from enum import Enum
 
 steamID = "STEAM_0:0:121430886"
 
-myApi_key='f9274211f281c4ceba5ce109481d25c5'
-myApi_secret='4b84310cca4ec4fb2d8138c65811dcfc04fdaaa3d3c44b6187820dc37fc38c5f'
-myToken='ATTAedd540d14edadf2f5eac6bb4570dec3fdb49e968f7511c152bf7f816838e0b62C0364953'
-token_secret=''
+
 
 client = TrelloClient(
-    api_key= myApi_key,
-    api_secret= myApi_secret,
-    token= myToken,
+    api_key= config.__myApi_key__,
+    api_secret= config.__myApi_secret__,
+    token= config.__myToken__,
     #token_secret='your-oauth-token-secret'
 )
 
---
+++ PythonAPISide/config.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+__boardid__ = '8hSBNmno'
+
+__myApi_key__='f9274211f281c4ceba5ce109481d25c5'
+__myApi_secret__='4b84310cca4ec4fb2d8138c65811dcfc04fdaaa3d3c44b6187820dc37fc38c5f'
+__myToken__='ATTA06188c7b9f6b5a9f913b887cb97859b76cc549e1a0902072395a421e25ead2f52828FAC5'
+__token_secret__=''
+
+__ranks__ = Enum(['SR','AsO','O','SO','I','SI','SGT','SSGT','LT','SLT','SM','Chief'])
