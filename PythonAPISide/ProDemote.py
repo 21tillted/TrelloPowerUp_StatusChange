a31b96c270824e7ff4712175b5faf716edbce858
21tillted <tilltb.brandt@gmail.com>
2023-04-17 12:56:52 +0200

    first real structure

--- PythonAPISide/ProDemote.py
+++ PythonAPISide/ProDemote.py
@@ -15,31 +15,30 @@ client = TrelloClient(
     #token_secret='your-oauth-token-secret'
 )
 
-all_boards = client.list_boards()
-last_board = all_boards[-1]
-
-currentBoard = client.get_board('8hSBNmno')
-#cards = currentBoard.get_cards('all','all')
+currentBoard = client.get_board(config.__boardid__)
 
 # Get all the list in the board 
 all_lists = currentBoard.open_lists() 
 df = pd.DataFrame()
-# Get list name and cards in it (Card name) 
-for list in all_lists: 
- for card in list.list_cards(): 
-   dict = {} 
-   df1 = pd.DataFrame() 
-   if not card.closed: 
-     dict['Card Name']=[card.name]
-     dict['List Name']=[list.name] 
-     # Get the custom fields
-     for custom_fields in card.custom_fields: 
-       dict[custom_fields.name]= [custom_fields.value]
- 
-     # Create a data frame with card ,list , custom field in card
-     df1 = pd.DataFrame(dict) 
-     # Merge the dataframe in order to add all card's custom fields
-     df =pd.concat([df, df1], ignore_index=True, sort=False) 
+# Get list name and cards in it (Card name)
+for list in all_lists[1:]:
+ for card in list.list_cards():
+  
+  if not card.name.__contains__('-'):
+    print(card)
+  # dict = {}
+  # df1 = pd.DataFrame()
+  # if not card.closed:
+  #   dict['Card Name']=[card.name]
+  #   dict['List Name']=[list.name] 
+  #   # Get the custom fields
+  #   for custom_fields in card.custom_fields: 
+  #     dict[custom_fields.name]= [custom_fields.value]
+
+  #   # Create a data frame with card ,list , custom field in card
+  #   df1 = pd.DataFrame(dict) 
+  #   # Merge the dataframe in order to add all card's custom fields
+  #   df =pd.concat([df, df1], ignore_index=True, sort=False) 
 
 
 #Forlater
--- PythonAPISide/config.py
+++ PythonAPISide/config.py
@@ -7,4 +7,4 @@ __myApi_secret__='4b84310cca4ec4fb2d8138c65811dcfc04fdaaa3d3c44b6187820dc37fc38c
 __myToken__='ATTA06188c7b9f6b5a9f913b887cb97859b76cc549e1a0902072395a421e25ead2f52828FAC5'
 __token_secret__=''
 
-__ranks__ = Enum(['SR','AsO','O','SO','I','SI','SGT','SSGT','LT','SLT','SM','Chief'])
+#__ranks__ = Enum(['SR','AsO','O','SO','I','SI','SGT','SSGT','LT','SLT','SM','Chief'])
